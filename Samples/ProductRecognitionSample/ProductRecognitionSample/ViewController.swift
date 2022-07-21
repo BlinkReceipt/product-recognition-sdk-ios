a6ae07b6b852f76f2859a59d0136bb8e6d414c2b
Ivan Martinović <martinovic1412@gmail.com>
2022-07-21 12:59:16 +0200

    Sample app initial version

---
+++ Samples/ProductRecognitionSample/ProductRecognitionSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProductRecognitionSample
+//
+//  Created by Ivan Martinović on 21.07.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
