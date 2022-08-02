91aad48f12b30fd7dd61a97e1b56b79595dfa301
J Andy Lim <104969078+j-andylim@users.noreply.github.com>
2022-08-02 15:47:26 +0800

    Add ProductRecognition xcframework

---
+++ ProductRecognition.xcframework/ios-arm64/ProductRecognition.framework/Headers/ProductRecognition.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2022 Microblink Ltd. All rights reserved.
+//
+// ANY UNAUTHORIZED USE OR SALE, DUPLICATION, OR DISTRIBUTION
+// OF THIS PROGRAM OR ANY OF ITS PARTS, IN SOURCE OR BINARY FORMS,
+// WITH OR WITHOUT MODIFICATION, WITH THE PURPOSE OF ACQUIRING
+// UNLAWFUL MATERIAL OR ANY OTHER BENEFIT IS PROHIBITED!
+// THIS PROGRAM IS PROTECTED BY COPYRIGHT LAWS AND YOU MAY NOT
+// REVERSE ENGINEER, DECOMPILE, OR DISASSEMBLE IT.
+
+#import <Foundation/Foundation.h>
+#import "FeaturesExtractorObjC.h"
+#import "ImageProcessor.h"
+#import "RGBImageCropper.h"
+#import "AddProductsViewController.h"
+#import "TrackedDataObjC.h"
+
+//! Project version number for ProductRecognition.
+FOUNDATION_EXPORT double ProductRecognitionVersionNumber;
+
+//! Project version string for ProductRecognition.
+FOUNDATION_EXPORT const unsigned char ProductRecognitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProductRecognition/PublicHeader.h>
